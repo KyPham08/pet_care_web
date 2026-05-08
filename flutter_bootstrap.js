f1b42e239cdd281065d916137d544c7c2bd0319f
key <phamkypikachu@gmail.com>
2026-05-08 18:11:46 +0700

    Update web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1459960787"
+    serviceWorkerVersion: "1409535095"
   }
 });
